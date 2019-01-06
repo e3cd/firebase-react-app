d16bc82431f10390e24e1eb7a12ad01f3aff38f4
Allan Gao <allangao95@gmail.com>
2019-01-06 18:18:08 +1100

    test

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo" style={{ textAlign: "left" }}>
-          Firebase
+          FireBase
         </Link>
 
         <SignedInLinks />
